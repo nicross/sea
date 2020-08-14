d976bacd31d2a27ffecaf758b6cb47fb388aa1ba
nicross <nicross@users.noreply.github.com>
2020-08-14 15:22:57 -0500

    Outline basic screen flow

---
+++ src/js/app/screen/game.js
@@ -0,0 +1,39 @@
+app.screen.game = (() => {
+  let root
+
+  function handleControls() {
+    // TODO
+  }
+
+  function onEnter() {
+    app.utility.focus.set(root)
+    engine.loop.on('frame', onFrame)
+
+    engine.audio.ramp.linear(engine.audio.mixer.master.param.gain, 1, 1)
+    engine.loop.resume()
+  }
+
+  function onExit() {
+    engine.loop.pause()
+    engine.loop.off('frame', onFrame)
+    engine.audio.ramp.linear(engine.audio.mixer.master.param.gain, engine.const.zeroGain, 1)
+  }
+
+  function onFrame(e) {
+    handleControls(e)
+  }
+
+  return {
+    activate: function () {
+      root = document.querySelector('.a-game')
+      app.utility.focus.trap(root)
+
+      app.state.screen.on('enter-game', onEnter)
+      app.state.screen.on('exit-game', onExit)
+
+      return this
+    },
+  }
+})()
+
+app.once('activate', () => app.screen.game.activate())

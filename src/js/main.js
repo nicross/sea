7ada63aa0f3f6b37a4591646f05b2c97e6baeaf2
nicross <nicross@users.noreply.github.com>
2020-08-15 16:36:01 -0500

    Set up basic limiter and reverb settings

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   engine.loop.start().pause()
   app.activate()
+
+  engine.audio.mixer.master.param.limiter.attack.value = 0.003
+  engine.audio.mixer.master.param.limiter.gain.value = 1
+  engine.audio.mixer.master.param.limiter.knee.value = 15
+  engine.audio.mixer.master.param.limiter.ratio.value = 15
+  engine.audio.mixer.master.param.limiter.release.value = 0.125
+  engine.audio.mixer.master.param.limiter.threshold.value = -30
+
+  engine.audio.mixer.auxiliary.reverb.setImpulse(engine.audio.buffer.impulse.large())
+  engine.audio.mixer.auxiliary.reverb.setGain(engine.utility.fromDb(-3))
 })

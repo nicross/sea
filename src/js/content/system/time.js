bd09cd2ea77f1aca6aa15d23612c7bd3361f7d0e
nicross <nicross@users.noreply.github.com>
2020-08-15 11:58:34 -0500

    Add content.system.time

---
+++ src/js/content/system/time.js
@@ -0,0 +1,28 @@
+content.system.time = (() => {
+  let time = 0
+
+  return {
+    get: function () {
+      return time
+    },
+    increment: function (value) {
+      time += value
+      return this
+    },
+    set: function (value) {
+      time = Number(value) || 0
+      return this
+    },
+  }
+})()
+
+engine.state.on('import', ({time}) => content.system.time.set(time))
+engine.state.on('export', (data) => data.time = content.system.time.get())
+
+engine.loop.on('frame', ({delta, paused}) => {
+  if (paused) {
+    return
+  }
+
+  content.system.time.increment(delta)
+})

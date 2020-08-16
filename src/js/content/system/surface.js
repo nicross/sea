8af6af711c19ce3392a54c8065eb9fe23594cee2
nicross <nicross@users.noreply.github.com>
2020-08-16 14:22:14 -0500

    Add height to surface

--- src/js/content/system/surface.js
+++ src/js/content/system/surface.js
@@ -1,9 +1,13 @@
 content.system.surface = (() => {
   const field = engine.utility.createPerlinWithOctaves(engine.utility.perlin3d, 'surface', 4),
     physicalScale = 10, // Nodes are 10m apart
-    timeScale = 60 // Evolves over 60 seconds
+    timeScale = 60, // Evolves over 60 seconds
+    waveHeight = 2 // m
 
   return {
+    height: function (x, y) {
+      return this.value(x, y) * waveHeight
+    },
     reset: function () {
       field.reset()
       return this

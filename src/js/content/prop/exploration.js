3d645cf8714a5fcde7491a93900b78c3b009cf6c
nicross <nicross@users.noreply.github.com>
2020-08-23 18:58:11 -0500

    Reduce temporary exploration cue volume

--- src/js/content/prop/exploration.js
+++ src/js/content/prop/exploration.js
@@ -4,7 +4,7 @@ content.prop.exploration = content.prop.base.invent({
     // TODO: Implement real sound, e.g. periodic glitter, possibly via a service that loops through all nodes and tells them what notes to play (e.g. for performance and a cool effect)
     this.synth = engine.audio.synth.createSimple({
       frequency: engine.utility.random.float(220, 440),
-      gain: 0.25,
+      gain: engine.utility.fromDb(-18),
     }).connect(this.output.input)
 
     content.system.audio.exploration.addProp(this)

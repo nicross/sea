a86aee48fb15ca3aefef0b044551564f46646d5f
nicross <nicross@users.noreply.github.com>
2020-08-24 16:15:33 -0500

    Add harmonic glitter to scanned surfaces

--- src/js/content/prop/exploration.js
+++ src/js/content/prop/exploration.js
@@ -1,13 +1,31 @@
 content.prop.exploration = content.prop.base.invent({
   name: 'Exploration Node',
-  onConstruct: function () {
-    content.system.audio.exploration.addProp(this)
+  glitter: function () {
+    const strength = Math.random()
+
+    const duration = engine.utility.lerp(0.5, 2, strength),
+      frequency = engine.utility.choose(content.system.soundtrack.harmonics(), Math.random()),
+      gain = engine.utility.fromDb(engine.utility.lerp(-24, -18, strength))
+
+    const synth = engine.audio.synth.createSimple({
+      frequency,
+    }).connect(this.output.input)
+
+    const now = engine.audio.time()
+
+    synth.param.gain.setValueAtTime(engine.const.zeroGain, now)
+    synth.param.gain.linearRampToValueAtTime(gain, now + duration/2)
+    synth.param.gain.linearRampToValueAtTime(engine.const.zeroGain, now + duration)
+
+    synth.stop(now + duration)
+
+    return engine.utility.timing.promise(duration * 1500)
   },
-  onDestroy: function () {
-    content.system.audio.exploration.removeProp(this)
-  },
-  trigger: function (frequency) {
-    // TODO: Build synth
-    // TODO: Find further optimizations, like only updating binaural while triggering
+  onUpdate: function () {
+    this.handlePeriodic({
+      delay: () => this.hasPeriodic('glitter') ? engine.const.zeroTime : 1,
+      key: 'glitter',
+      trigger: () => this.glitter(),
+    })
   },
 })

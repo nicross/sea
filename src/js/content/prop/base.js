4f205ce0b73893b5f8ce0869b7fc486568ed7ab1
nicross <nicross@users.noreply.github.com>
2020-12-26 15:51:15 -0600

    Set base prop reverb to false

--- src/js/content/prop/base.js
+++ src/js/content/prop/base.js
@@ -1,54 +1,13 @@
 content.prop.base = engine.prop.base.invent((prototype) => ({
   construct: function ({
-    destination = engine.audio.mixer.bus.props(),
-    radius = this.radius || 0,
-    token,
-    x = 0,
-    y = 0,
-    z = 0,
-    ...options
+    destination,
   } = {}) {
-    // Copied but without reverb
-    const context = engine.audio.context()
-
     this.destination = destination
-    this.instantiated = true
-    this.periodic = {}
-    this.radius = radius
-    this.spawnAngle = this.angle
-    this.token = token
-    this.x = x
-    this.y = y
-    this.z = z
-
-    this.binaural = engine.audio.binaural.create()
-    this.output = context.createGain()
-
-    this.binaural.from(this.output)
-    this.binaural.to(destination)
-
-    this.output.gain.value = engine.const.zeroGain
-    engine.audio.ramp.linear(this.output.gain, 1, engine.const.propFadeDuration)
-
-    engine.utility.physical.decorate(this)
-
-    this.recalculate()
-    this.onConstruct(options)
-
-    return this
+    return prototype.construct.apply(this, arguments)
   },
-  destroy: function () {
-    // Copied but without reverb
-    engine.audio.ramp.linear(this.output.gain, engine.const.zeroGain, engine.const.propFadeDuration)
-
-    setTimeout(() => {
-      this.output.disconnect()
-      this.binaural.destroy()
-      this.onDestroy()
-    }, engine.const.propFadeDuration * 1000)
-
-    return this
-  },
+  fadeInDuration: 1/8,
+  fadeOutDuration: 1/8,
+  // TODO: Port removed periodic logic
   rebuildBinaural: function () {
     this.binaural.destroy()
 
@@ -60,22 +19,5 @@ content.prop.base = engine.prop.base.invent((prototype) => ({
 
     return this
   },
-  recalculate: function () {
-    // Copied but without reverb
-    const positionQuaternion = engine.position.getQuaternion(),
-      positionVector = engine.position.getVector()
-
-    this.updatePhysics()
-
-    this.relative = this.vector()
-      .subtract(positionVector)
-      .subtractRadius(this.radius)
-      .rotateQuaternion(positionQuaternion.conjugate())
-
-    this.distance = this.relative.distance()
-
-    this.binaural.update({...this.relative})
-
-    return this
-  },
+  reverb: false,
 }))

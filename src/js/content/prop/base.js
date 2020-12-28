b2c03cd6c108cd1dc8f26203fd83d2559091b541
nicross <nicross@users.noreply.github.com>
2020-12-27 18:53:56 -0600

    Reinstate periodic prop behaviors

--- src/js/content/prop/base.js
+++ src/js/content/prop/base.js
@@ -3,11 +3,53 @@ content.prop.base = engine.prop.base.invent((prototype) => ({
     destination,
   } = {}) {
     this.destination = destination
+    this.periodic = {}
     return prototype.construct.apply(this, arguments)
   },
   fadeInDuration: 1/8,
   fadeOutDuration: 1/8,
-  // TODO: Port removed periodic logic
+  handlePeriodic: function ({
+    delay = () => 0,
+    key = '',
+    trigger = () => Promise.resolve(),
+  } = {}) {
+    if (!(key in this.periodic)) {
+      this.periodic[key] = {
+        active: false,
+        timer: delay() * Math.random(),
+      }
+    }
+
+    const periodic = this.periodic[key]
+
+    if (periodic.active) {
+      return this
+    }
+
+    if (periodic.timer < 0) {
+      periodic.timer = delay()
+    }
+
+    periodic.timer -= engine.loop.delta()
+
+    if (periodic.timer <= 0) {
+      const result = trigger() || Promise.resolve()
+      periodic.active = true
+      periodic.timer = -Infinity // XXX: Force delay() next inactive frame
+      result.then(() => periodic.active = false)
+    }
+
+    return this
+  },
+  hasPeriodic: function (key) {
+    return key in this.periodic
+  },
+  isPeriodicActive: function (key) {
+    return this.periodic[key] && this.periodic[key].active
+  },
+  isPeriodicPending: function (key) {
+    return this.periodic[key] && !this.periodic[key].active
+  },
   rebuildBinaural: function () {
     this.binaural.destroy()
 
@@ -19,5 +61,8 @@ content.prop.base = engine.prop.base.invent((prototype) => ({
 
     return this
   },
-  reverb: false,
+  resetPeriodic: function (key) {
+    delete this.periodic[key]
+    return this
+  },
 }))

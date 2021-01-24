ff6592a672816cb6f17c085f5a40890e6629347a
nicross <nicross@users.noreply.github.com>
2021-01-24 12:39:58 -0600

    Outline compass

---
+++ src/js/app/screen/game/canvas/hud/compass.js
@@ -0,0 +1,10 @@
+app.screen.game.canvas.hud.compass = (() => {
+  app.screen.game.canvas.hud.on('draw', draw)
+
+  function draw({canvas, context}) {
+    // Get screen coordinates for compass rose points
+    // Draw visible elements with decreasing opacity from top-center of screen
+  }
+
+  return {}
+})()

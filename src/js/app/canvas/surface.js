7526b6d661e6cdde8f0ff4f755b29db9c1e49bd2
nicross <nicross@users.noreply.github.com>
2021-08-07 00:00:22 -0500

    Optimize terrain rendering by rendering points within a cone ahead of the camera

--- src/js/app/canvas/surface.js
+++ src/js/app/canvas/surface.js
@@ -2,10 +2,58 @@ app.canvas.surface = (() => {
   const canvas = document.createElement('canvas'),
     context = canvas.getContext('2d'),
     main = app.canvas,
+    maxDrawDistance = 35,
     shimmerField = engine.utility.simplex3d.create('shimmer'),
     shimmerScaleX = 2 / engine.utility.simplex3d.prototype.skewFactor,
     shimmerScaleY = 2 / engine.utility.simplex3d.prototype.skewFactor,
     shimmerScaleZ = 0.5 / engine.utility.simplex3d.prototype.skewFactor
+
+  /**
+   * The gridCache allows quick lookup for the vertices to draw given a heading and horizontal field of view.
+   * Vertices are converted to polar coordinates and indexed by their angle within a bitree.
+   * For simplicity, three complete rotations of the grid are stored so values always exist for every possible input.
+   * Coordinates for vertices are expressed relative to the camera.
+   */
+  const gridCache = engine.utility.bitree.create({
+    dimension: 'angle',
+    maxItems: maxDrawDistance,
+    minValue: -engine.const.tau * 1.5,
+    range: engine.const.tau * 3,
+  })
+
+  // Fill gridCache to maximum draw distance
+  for (let x = -maxDrawDistance; x < maxDrawDistance; x += 1) {
+    for (let y = -maxDrawDistance; y < maxDrawDistance; y += 1) {
+      const distance = Math.sqrt((x * x) + (y * y))
+
+      if (distance > maxDrawDistance) {
+        continue
+      }
+
+      const angle = Math.atan2(y, x)
+
+      gridCache.insert({
+        angle: angle - engine.const.tau,
+        distance,
+        x,
+        y,
+      })
+
+      gridCache.insert({
+        angle,
+        distance,
+        x,
+        y,
+      })
+
+      gridCache.insert({
+        angle: angle + engine.const.tau,
+        distance,
+        x,
+        y,
+      })
+    }
+  }
 
   let drawDistance,
     nodeRadius
@@ -31,88 +79,59 @@ app.canvas.surface = (() => {
 
   function drawNodes() {
     const color = getColor(),
-      heading = engine.utility.vector3d.unitX().rotateQuaternion(engine.position.getQuaternion().conjugate()),
-      height = main.height(),
+      heading = engine.utility.vector3d.unitX().rotateQuaternion(engine.position.getQuaternion()),
+      headingConjugate = engine.utility.vector3d.unitX().rotateQuaternion(engine.position.getQuaternion().conjugate()),
       hfov = main.hfov(),
+      hfovLeeway = hfov / 8,
       position = engine.position.getVector(),
       positionGrid = position.clone(),
-      rotateYaw = Math.atan2(heading.y, heading.x),
+      rotateYaw = Math.atan2(headingConjugate.y, headingConjugate.x),
       time = content.time.value(),
-      vfov = main.vfov(),
-      width = main.width(),
+      vertices = gridCache.retrieve(Math.atan2(heading.y, heading.x) - ((hfov + hfovLeeway) / 2), hfov + hfovLeeway),
       zOffset = engine.const.positionRadius / 2
 
     positionGrid.x = Math.round(positionGrid.x)
     positionGrid.y = Math.round(positionGrid.y)
 
-    // TODO: Optimize further, possibly by selecting a sector from a pre-computed disc of relative points based on heading/hfov (a bitree could be useful), converting to global space
-    for (let x = -drawDistance; x < drawDistance; x += 1) {
-      for (let y = -drawDistance; y < drawDistance; y += 1) {
-        // Convert to relative space
-        const global = positionGrid.add({x, y})
+    for (const vertex of vertices) {
+      // Convert to relative space
+      const global = positionGrid.add(vertex)
 
-        const relative = engine.utility.vector3d.create(
-          engine.utility.vector2d.create({
-            x: global.x - position.x,
-            y: global.y - position.y,
-          }).rotate(rotateYaw)
-        )
+      const relative = engine.utility.vector3d.create(
+        engine.utility.vector2d.create({
+          x: global.x - position.x,
+          y: global.y - position.y,
+        }).rotate(rotateYaw)
+      )
 
-        // Filter out nodes behind field of view
-        if (relative.x <= 0) {
-          continue
-        }
+      // Calculate true position
+      global.z = content.surface.value(global.x, global.y)
+      relative.z = global.z - (position.z + zOffset)
 
-        const hangle = Math.atan2(relative.y, relative.x)
+      // Calculate true distance
+      const distance = relative.distance()
 
-        // Filter out nodes beyond horizontal field of view (with leeway)
-        if (Math.abs(hangle) > hfov / 1.95) {
-          continue
-        }
-
-        // Filter out nodes too distant to see
-        if (relative.distance() > drawDistance) {
-          continue
-        }
-
-        // Calculate z-coordinate and vertical angle
-        relative.z = content.surface.value(global.x, global.y) - (position.z + zOffset)
-
-        const vangle = Math.atan2(relative.z, relative.x)
-
-        // Filter out nodes beyond vertical field of view (with leeway)
-        if (Math.abs(vangle) > vfov / 1.95) {
-          continue
-        }
-
-        // Calculate true distance with known z-coordinate
-        const distance = relative.distance()
-
-        // Filter out nodes too distant to see
-        if (distance > drawDistance) {
-          continue
-        }
-
-        // Calculate position in screen space
-        const screen = engine.utility.vector2d.create({
-          x: (width / 2) - (width * hangle / hfov),
-          y: (height / 2) - (height * vangle / vfov),
-        })
-
-        // Calculate properties
-        const alphaRatio = engine.utility.scale(distance, 0, drawDistance, 1, 0),
-          radiusRatio = engine.utility.scale(distance, 0, 35, 1, 0), // max drawDistance
-          radius = engine.utility.lerpExp(1, nodeRadius, radiusRatio, 12),
-          shimmer = getShimmer(global.x, global.y, time)
-
-        // Apply shimmer to color
-        const alpha = engine.utility.clamp(color.a * engine.utility.lerp(0.5, 1.5, shimmer), 0, 1) * (alphaRatio ** 0.666),
-          luminance = engine.utility.clamp(color.l * engine.utility.lerp(1.5, 0.5, shimmer), 0, 1)
-
-        // Draw
-        context.fillStyle = `hsla(${color.h}, ${color.s}%, ${luminance * 100}%, ${alpha})`
-        context.fillRect(screen.x - radius, screen.y - radius, radius * 2, radius * 2)
+      // Optimization: only draw within draw distance
+      if (distance > drawDistance) {
+        continue
       }
+
+      // Convert to screen space
+      const screen = main.toScreenFromRelative(relative)
+
+      // Calculate properties
+      const alphaRatio = engine.utility.scale(distance, 0, drawDistance, 1, 0),
+        radiusRatio = engine.utility.scale(distance, 0, maxDrawDistance, 1, 0),
+        radius = engine.utility.lerpExp(1, nodeRadius, radiusRatio, 12),
+        shimmer = getShimmer(global.x, global.y, time)
+
+      // Apply shimmer to color
+      const alpha = engine.utility.clamp(color.a * engine.utility.lerp(0.5, 1.5, shimmer), 0, 1) * (alphaRatio ** 0.666),
+        luminance = engine.utility.clamp(color.l * engine.utility.lerp(1.5, 0.5, shimmer), 0, 1)
+
+      // Draw
+      context.fillStyle = `hsla(${color.h}, ${color.s}%, ${luminance * 100}%, ${alpha})`
+      context.fillRect(screen.x - radius, screen.y - radius, radius * 2, radius * 2)
     }
   }
 

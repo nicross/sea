680b544ed1b9af2db73ba495c660fd3fd0ae6d5c
nicross <nicross@users.noreply.github.com>
2021-10-16 18:41:02 -0500

    Fix starting loop until app is ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,6 @@
-engine.ready(() => {
+app.ready(() => {
   engine.loop.start()
   app.activate()
-
-  // XXX: Prevent unexpected audio, e.g. if initialization takes long and pausedVolume is zero
-  engine.audio.mixer.master.param.gain.value = engine.const.zeroGain
-
-  // Set a boosted dynamic range
-  engine.audio.mixer.master.param.limiter.attack.value = 0.003
-  engine.audio.mixer.master.param.limiter.gain.value = 1
-  engine.audio.mixer.master.param.limiter.knee.value = 15
-  engine.audio.mixer.master.param.limiter.ratio.value = 15
-  engine.audio.mixer.master.param.limiter.release.value = 0.125
-  engine.audio.mixer.master.param.limiter.threshold.value = -30
 
   if (!app.isElectron()) {
     window.addEventListener('beforeunload', (e) => {
@@ -22,3 +11,14 @@ engine.ready(() => {
     })
   }
 })
+
+// XXX: Prevent unexpected audio, e.g. if initialization takes long and pausedVolume is zero
+engine.audio.mixer.master.param.gain.value = engine.const.zeroGain
+
+// Set a boosted dynamic range
+engine.audio.mixer.master.param.limiter.attack.value = 0.003
+engine.audio.mixer.master.param.limiter.gain.value = 1
+engine.audio.mixer.master.param.limiter.knee.value = 15
+engine.audio.mixer.master.param.limiter.ratio.value = 15
+engine.audio.mixer.master.param.limiter.release.value = 0.125
+engine.audio.mixer.master.param.limiter.threshold.value = -30

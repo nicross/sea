nicross/sea

commit 740595bc22d8613007f4fae5a64a74535e2d4c33

Use array for storage keyPath to create separate rows for versioned data

src/js/app/storage.js
@@ -7,9 +7,7 @@ app.storage = (() => {
   }
 
   function set(key, value) {
-    const data = app.storage.api.get(version) || {}
-    data[key] = value
-    app.storage.api.set(version, data)
+    app.storage.api.set(version, key, value)
   }
 
   return {
@@ -47,7 +45,7 @@ app.storage = (() => {
     getVersions: function () {
       return [
         this.legacy.version(),
-        ...this.api.keys(),
+        ...this.api.versions(),
       ].filter(Boolean).sort((a, b) => {
         return app.utility.semver.compare(a, b)
       })

src/js/app/storage/api.js
@@ -1,22 +1,16 @@
 app.storage.api = (() => {
   const dbName = 'shiftbacktick_sea',
-    dbStoreName = 'versions',
-    dbStoreProxyKey = 'version',
-    dbStoreProxyValue = 'data',
+    dbStoreName = 'data',
     dbVersion = 1,
+    debounceHandlers = {},
     isSupported = 'indexedDB' in window,
-    ready = open(),
-    setDbDebounced = app.utility.fn.debounced(setDb)
+    proxy = {},
+    ready = open()
 
-  let db,
-    proxy = {}
+  let db
 
-  function get(key) {
-    return proxy[key]
-  }
-
-  function keys() {
-    return [...Object.keys(proxy)]
+  function get(version, key) {
+    return key ? proxy[version][key] : proxy[version]
   }
 
   function open() {
@@ -36,7 +30,9 @@ app.storage.api = (() => {
       }
 
       request.onupgradeneeded = () => {
-        request.result.createObjectStore(dbStoreName, {keyPath: dbStoreProxyKey})
+        request.result.createObjectStore(dbStoreName, {
+          keyPath: ['version', 'key'],
+        })
       }
     })
   }
@@ -54,11 +50,12 @@ app.storage.api = (() => {
       request.onsuccess = () => {
         const results = request.result
 
-        for (const result of results) {
-          const key = result[dbStoreProxyKey],
-            value = result[dbStoreProxyValue]
+        for (const {data, key, version} of results) {
+          if (!proxy[version]) {
+            proxy[version] = {}
+          }
 
-          proxy[key] = value
+          proxy[version][key] = data
         }
 
         resolve()
@@ -66,33 +63,51 @@ app.storage.api = (() => {
     })
   }
 
-  function set(key, value) {
-    proxy[key] = value
+  function set(version, key, data) {
+    if (!proxy[version]) {
+      proxy[version] = {}
+    }
+
+    proxy[version][key] = data
 
     if (!isSupported) {
       return
     }
 
-    setDbDebounced(key, value)
+    setDbDebounced(version, key, data)
   }
 
-  function setDb(key, value) {
-    const item = {}
-    item[dbStoreProxyKey] = key
-    item[dbStoreProxyValue] = value
+  function setDb(version, key, data) {
+    const item = {
+      data,
+      key,
+      version,
+    }
 
     db.transaction([dbStoreName], 'readwrite').objectStore(dbStoreName).put(item)
   }
 
+  function setDbDebounced(version, key, data) {
+    if (!debounceHandlers[version]) {
+      debounceHandlers[version] = {}
+    }
+
+    clearTimeout(debounceHandlers[version][key])
+
+    debounceHandlers[version][key] = setTimeout(() => {
+      setDb(version, key, data)
+    }, 0)
+  }
+
   return {
-    get: (key) => {
-      return get(key)
+    get: (version, key) => {
+      return get(version, key)
     },
-    keys: () => keys(),
     ready: () => ready,
-    set: (key, value) => {
-      set(key, value)
+    set: (version, key, value) => {
+      set(version, key, value)
       return this
     },
+    versions: () => [...Object.keys(proxy)],
   }
 })()

commit c4c3a79441fb538e9643215986965f13e51405b7

Fix legacy storage elevation caused by 740595b

src/js/app/storage.js
@@ -16,7 +16,9 @@ app.storage = (() => {
         ? this.legacy.data()
         : app.storage.api.get(from)
 
-      this.api.set(to, data)
+      for (const key of Object.keys(data)) {
+        this.api.set(to, key, data[key])
+      }
 
       return this
     },

src/js/app/storage/legacy.js
@@ -15,14 +15,15 @@ app.storage.legacy = (() => {
     settings: 'sea_settings',
     stats: 'sea_stats',
     treasures: 'sea_treasures',
-    version: 'sea_version',
   }
+
+  const versionKey = 'sea_version'
 
   function get(key) {
     try {
       const value = api.getItem(key)
 
-      return key == keys.version
+      return key == versionKey
         ? value
         : JSON.parse(value)
     } catch (e) {}
@@ -38,6 +39,6 @@ app.storage.legacy = (() => {
 
       return data
     },
-    version: () => get(keys.version),
+    version: () => get(versionKey),
   }
 })()
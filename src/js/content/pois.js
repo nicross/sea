30465ac555910a9903543fb8157b642e45d94ddb
nicross <nicross@users.noreply.github.com>
2021-10-22 01:36:09 -0500

    Create a basic point of interest system for tracking discovered worms (and potential future POIs)

---
+++ src/js/content/pois.js
@@ -0,0 +1,69 @@
+content.pois = (() => {
+  const pois = [],
+    pubsub = engine.utility.pubsub.create(),
+    wormIds = new Set()
+
+  function wormToPoi(worm) {
+    const first = worm.points[0]
+
+    return {
+      id: worm.getId(),
+      name: content.utility.nato.fromNumber(wormIds.size + 1),
+      type: 'worm',
+      x: first.x,
+      y: first.y,
+      z: first.z,
+    }
+  }
+
+  return engine.utility.pubsub.decorate({
+    export: () => [...pois],
+    import: function (data = []) {
+      for (const poi of data) {
+        pois.push(poi)
+
+        if (poi.type == 'worm') {
+          wormIds.add(poi.id)
+        }
+      }
+
+      return this
+    },
+    onScanRecharge: function ({
+      worms: scannedWorms = [],
+    }) {
+      // Add newly scanned worms
+      for (const worm of scannedWorms) {
+        const id = worm.getId()
+
+        if (wormIds.has(id)) {
+          continue
+        }
+
+        const poi = wormToPoi(worm)
+
+        pois.push(poi)
+        wormIds.add(id)
+
+        pubsub.emit('discover', poi)
+        pubsub.emit('worm', poi)
+      }
+
+      return this
+    },
+    reset: function () {
+      pois.length = 0
+      wormIds.clear()
+
+      return this
+    },
+  }, pubsub)
+})()
+
+engine.ready(() => {
+  content.scan.on('recharge', (e) => content.pois.onScanRecharge(e))
+})
+
+engine.state.on('export', (data) => data.pois = content.pois.export())
+engine.state.on('import', ({pois}) => content.pois.import(pois))
+engine.state.on('reset', () => content.pois.reset())
